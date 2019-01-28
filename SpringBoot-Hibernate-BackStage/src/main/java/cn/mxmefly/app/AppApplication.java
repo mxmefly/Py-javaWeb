4980e7d73158ff11808e6194546c1773486f69bd
wzy <1713040517@qq.com>
2019-01-28 17:23:37 +0800

    上传框架

---
+++ SpringBoot-Hibernate-BackStage/src/main/java/cn/mxmefly/app/AppApplication.java
@@ -0,0 +1,14 @@
+package cn.mxmefly.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
+
